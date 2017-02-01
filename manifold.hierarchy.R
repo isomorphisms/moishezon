379dcdf3b56ecb22d115dbcf11f87ad043a6dc4a
isomorphismes <isomorphisms@sdf.org>
2017-02-01 16:48:16 -0500

    this is wrong

--- manifold.hierarchy.R
+++ manifold.hierarchy.R
@@ -22,3 +22,14 @@ warning("This seems wrong because of stackoverflow.com/questions/8831910. I want
 
 
 message("Kähler and Moishezon together imply ℙ algebraic.")
+
+
+
+message("Maybe a better way to do this is to make each adjective:")
+setClass("smooth")
+setClass("compact"
+
+
+
+
+message("Part of me suspects, though, that all the mathematics needs to be *behind* the code, not the code itself. Thinking of ?orthoPoly , written at bell labs 1976 --- it doesn’t include the paper derivation, it’s just the result. Same with CONST::PI. We just define it in code and don’t talk about it or where it came from. Well, proceeding ahead anyway in case the mistakes themselves are productive……")
