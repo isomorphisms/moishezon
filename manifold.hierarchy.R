b639649133d4a2f69c2d8ce26bbe4ce1cdd8546f
isomorphismes <isomorphisms@sdf.org>
2017-02-01 16:45:33 -0500

    implications between adjectives

--- manifold.hierarchy.R
+++ manifold.hierarchy.R
@@ -9,3 +9,16 @@ setClass('CompactManifold', contains='Manifold')
 
 setClass('SmoothCompactManifold', contains='CompactManifold', 'SmoothManifold')
 warning("This seems wrong because of stackoverflow.com/questions/8831910. I want to say that smooth, compact manifolds are both smooth and compact -- not that method lookup should check in two places.")
+
+
+setClass("Hodge")
+setClass("Kähler", contains="Hodge")
+setClass("Moishezon")
+
+setClass("projective")
+setClass("algebraic")
+setClass("ℙ algebraic", contains=c("algebraic","projective"))
+warning("This seems wrong because of stackoverflow.com/questions/8831910. I want to say that smooth, compact manifolds are both smooth and compact -- not that method lookup should check in two places.")
+
+
+message("Kähler and Moishezon together imply ℙ algebraic.")
